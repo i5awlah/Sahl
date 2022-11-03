7866363b03f48a8c1540af519c221db9fd5ec7ba
Khawlah <khawlah_alrashed@hotmail.com>
2022-11-03 08:47:43 +0300

    Initial Commit

---
+++ Sahl/SahlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahlApp.swift
+//  Sahl
+//
+//  Created by Khawlah on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SahlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
